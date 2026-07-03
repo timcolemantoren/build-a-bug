8a22ad6380023ed9c45533560fc57b3b7df8304b
Toren <t@torenindex.com>
2026-07-03 17:03:24 -0400

    Add shared game constants

---
+++ src/shared/Config/GameConstants.lua
@@ -0,0 +1,15 @@
+--!strict
+
+local GameConstants = {
+	ExperienceName = "Build a Bug",
+	FirstModeName = "Backyard Survival",
+	CurrencyNames = {
+		Dna = "DNA",
+		Crumbs = "Crumbs",
+	},
+	Debug = {
+		AutoStartRoundInStudio = true,
+	},
+}
+
+return GameConstants
