8bcccedcff140cdf1e68b2204c55b9f3eaf4f52a
Toren <t@torenindex.com>
2026-07-03 17:03:09 -0400

    Add placeholder round end controller

---
+++ src/client/UI/RoundEndController.lua
@@ -0,0 +1,17 @@
+--!nonstrict
+
+-- Placeholder for future round-end rewards screen.
+-- For now, HUDController displays a simple round-ended message.
+
+local RoundEndController = {}
+
+function RoundEndController.Init(_remotes)
+	-- Future:
+	-- - show survived/eliminated state
+	-- - show DNA earned
+	-- - show crumbs collected
+	-- - show unlock progress
+	-- - offer Play Again / Return to Bug Lab buttons
+end
+
+return RoundEndController
