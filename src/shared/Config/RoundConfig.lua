bf8fcaa0b719e9999f16ba6541bd16b147baa615
Toren <t@torenindex.com>
2026-07-03 17:00:07 -0400

    Add round config

---
+++ src/shared/Config/RoundConfig.lua
@@ -0,0 +1,14 @@
+--!strict
+
+local RoundConfig = {
+	lobbyCountdownSeconds = 15,
+	roundDurationSeconds = 180,
+	minimumPlayers = 1,
+	crumbSpawnIntervalSeconds = 6,
+	crumbsPerSpawn = 6,
+	baseDnaReward = 10,
+	survivalDnaRewardPerMinute = 5,
+	crumbDnaReward = 1,
+}
+
+return RoundConfig
