be0d1544686e550e15ea642ce2adda6183276429
Toren <t@torenindex.com>
2026-07-03 21:06:53 -0400

    Apply ant crumb bonus to food rewards

--- src/server/RewardService.lua
+++ src/server/RewardService.lua
@@ -4,6 +4,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local BuildABugShared = ReplicatedStorage:WaitForChild("BuildABug")
 local RoundConfig = require(BuildABugShared.Config.RoundConfig)
+local BugArchetypes = require(BuildABugShared.Config.BugArchetypes)
 
 local RewardService = {}
 local PlayerDataService = nil
@@ -12,12 +13,27 @@ function RewardService.Init(playerDataService)
 	PlayerDataService = playerDataService
 end
 
+local function getEffectiveCrumbAmount(player: Player, baseAmount: number): number
+	if not PlayerDataService then
+		return baseAmount
+	end
+
+	local data = PlayerDataService.GetData(player)
+	local bug = data and BugArchetypes[data.selectedBug]
+	if not bug then
+		return baseAmount
+	end
+
+	return baseAmount + (bug.crumbCarryBonus or 0)
+end
+
 function RewardService.AwardCrumb(player: Player, amount: number?)
 	if not PlayerDataService then
 		return
 	end
 
-	local crumbAmount = amount or 1
+	local baseAmount = amount or 1
+	local crumbAmount = getEffectiveCrumbAmount(player, baseAmount)
 	PlayerDataService.AddCrumbs(player, crumbAmount)
 	PlayerDataService.AddDna(player, RoundConfig.crumbDnaReward * crumbAmount)
 end
