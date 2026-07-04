6b2974a69f870fd448b0f0aefc777b52c75914f0
Toren <t@torenindex.com>
2026-07-04 15:14:48 -0400

    Initialize environmental hazards

--- src/server/Main.server.lua
+++ src/server/Main.server.lua
@@ -11,6 +11,7 @@ local HazardService = require(script.Parent.HazardService)
 local ArenaService = require(script.Parent.ArenaService)
 local RoundService = require(script.Parent.RoundService)
 local AbilityService = require(script.Parent.AbilityService)
+local EnvironmentHazardService = require(script.Parent.EnvironmentHazardService)
 
 local function getOrCreateFolder(parent: Instance, name: string): Folder
 	local folder = parent:FindFirstChild(name)
@@ -53,5 +54,6 @@ RewardService.Init(PlayerDataService)
 HazardService.Init(remotes, PlayerDataService)
 RoundService.Init(remotes, PlayerDataService, RewardService, HazardService, ArenaService)
 AbilityService.Init(remotes, PlayerDataService)
+EnvironmentHazardService.Init(PlayerDataService)
 
 print("[Build a Bug] Server initialized")
