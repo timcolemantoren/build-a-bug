75b244b89ba29c1c0ab76dc846c8caf62c69d107
Toren <t@torenindex.com>
2026-08-28 14:54:58 -0400

    Add build preset remote

--- src/shared/Remotes/RemoteNames.lua
+++ src/shared/Remotes/RemoteNames.lua
@@ -7,6 +7,7 @@ local RemoteNames = {
 	FolderName = "BuildABugRemotes",
 	SelectBug = "SelectBug",
 	SelectBugLoadout = "SelectBugLoadout",
+	BuildPreset = "BuildPreset",
 	SetCosmetic = "SetCosmetic",
 	PurchaseCosmetic = "PurchaseCosmetic",
 	StartRoundRequest = "StartRoundRequest",
